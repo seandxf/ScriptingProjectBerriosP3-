4bdf0d4a62bda77dcefec0f4ffa0fad72c365cb0
Sean Berrios <722320@student.dusd.net>
2024-02-07 12:14:28 -0800

    tried to finish the script
    couldn't

---
+++ ScirptingProjectP3Berrios/Assets/scripts/LinearInterpolation.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearInterpolation : MonoBehaviour
+{
+    float result = Mathf.Lerp (3f, 5f, 0.5f);
+    Vector3 from = new Vector3 (1f, 2f, 3f);
+    Vector3 to = new Vector3 (5f, 6f, 7f);
+   
+    void Start()
+    {
+        Vector3 result = Vector3.Lerp(from, to, 0.75f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
