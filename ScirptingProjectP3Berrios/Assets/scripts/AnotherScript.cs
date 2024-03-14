23e31afee1cfe76152c495c64d60f201712bf5c0
Sean Berrios <722320@student.dusd.net>
2024-03-14 11:12:15 -0700

    did Mouse Down and get component
    sfgdsf

---
+++ ScirptingProjectP3Berrios/Assets/scripts/AnotherScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnotherScript : MonoBehaviour
+{
+    public int playerScore = 9001;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
